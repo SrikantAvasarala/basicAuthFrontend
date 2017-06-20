dc6411379b8da8c107bc6ab2018b5a9250d3a25d
Srikant Avasarala <srikanta@15MBP-14792.local>
2017-06-20 13:07:20 -0700

    login logout working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import {MaterialModule} from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { LoginService } from './services/login.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    FormsModule,
+    MaterialModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
